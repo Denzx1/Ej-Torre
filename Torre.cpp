183a925ffd5a84ef1458842abe984854ee990625
Denzx1 <denzuver@gmail.com>
2026-09-09 21:21:55 -0400

    Definir Constructor Torre

--- Torre.cpp
+++ Torre.cpp
@@ -6,7 +6,12 @@ private:
     int fila;
     int columna;
     bool isBlack;
-
+public:
+    Torre(int f, int c, bool black){
+        fila = f;
+        columna = c;
+        isBlack = black;
+    }
 };
 
 int main() {
