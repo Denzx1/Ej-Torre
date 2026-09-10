421ac1c93e29fb20668af97e2231d12cf0fe54eb
Denzx1 <denzuver@gmail.com>
2026-09-09 21:32:58 -0400

    Crear funcion validateMovement

--- Torre.cpp
+++ Torre.cpp
@@ -6,12 +6,24 @@ private:
     int fila;
     int columna;
     bool isBlack;
+    
+    bool validateMovement(int newFila, int newColumna){
+        if(newFila == fila || newColumna == columna){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 public:
     Torre(int f, int c, bool black){
         fila = f;
         columna = c;
         isBlack = black;
     }
+
+   
 };
 
 int main() {
